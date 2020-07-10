51643d05a2c0e2970f140cf1ab28c56c054b7199
Ram bhosale <rambhosale9@gmail.com>
2020-07-10 20:22:03 +0530

    Added: CodeIgniter Database source files
    Changed the namespace from "CodeIgniter\Database" to "OneMoreBlock\ORM"

---
+++ src/Exceptions/ExceptionInterface.php
@@ -0,0 +1,13 @@
+<?php namespace OneMoreBlock\ORM\Exceptions;
+
+/**
+ * Provides a domain-level interface for broad capture
+ * of all database-related exceptions.
+ *
+ * catch (\OneMoreBlock\ORM\Exceptions\ExceptionInterface) { ... }
+ */
+
+interface ExceptionInterface extends \CodeIgniter\Exceptions\ExceptionInterface
+{
+
+}
